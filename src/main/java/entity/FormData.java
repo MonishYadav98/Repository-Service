a687f58a2e1ac09789d7f44e6fd74bfc73184478
MonishYadav98 <monishyadav2798@gmail.com>
2025-01-20 10:51:30 +0530

    Create FormData.java

---
+++ src/main/java/entity/FormData.java
@@ -0,0 +1,30 @@
+package com.ttt.training.register.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+
+public class FormData {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String sid;
+	private String fullName;
+	private String email;
+	private String phoneNumber;
+	private String address;
+	private String universityStream;
+	private String courseDetails;
+	private double cgpa;
+	private int year;
+	
+	
+	
+
+}
